f94cac3863f3902871dfdfc88ef7a35b76025ae1
João Vaz <119084806+jg2alv@users.noreply.github.com>
2023-04-01 12:08:40 -0300

    Add proposed exercises & fix scanner bug

--- aeds1/livros/Fundamentos_da_Programacao_de_Computadores/Cap_3/Java.java
+++ aeds1/livros/Fundamentos_da_Programacao_de_Computadores/Cap_3/Java.java
@@ -1,113 +1,142 @@
 import java.util.*;
 
-class Main
-{
-    public static void ex_r_1()
-    {
-        double n1, n2, n3, n4, sum;
-        
-        try(Scanner scanner = new Scanner(System.in))
-        {
-            n1 = scanner.nextDouble();
-            n2 = scanner.nextDouble();
-            n3 = scanner.nextDouble();
-            n4 = scanner.nextDouble();
-            sum = n1 + n2 + n3 + n4;
-            
-            System.out.printf("%f", sum);
-        }
+class Main {
+    private Scanner scanner;
 
+    public Main() {
+        this.scanner = new Scanner(System.in);
     }
 
+    public void close() {
+        this.scanner.close();
+    }
 
-    public static void ex_r_5()
-    {
+    public void ex_r_05() {
         double sal, perc, nsal;
 
-        try(Scanner scanner = new Scanner(System.in))
-        {
-            sal = scanner.nextDouble();
-            perc = scanner.nextDouble();
-            nsal = sal * (perc / 100);
-        
-            System.out.printf("%f", nsal);
-        }
+        sal = this.scanner.nextDouble();
+        perc = this.scanner.nextDouble();
+        nsal = sal * (perc / 100);
+
+        System.out.printf("%f\n", nsal);
 
     }
 
-
-    public static void ex_r_10()
-    {
+    public void ex_r_10() {
         double radius, area;
 
-        try(Scanner scanner = new Scanner(System.in))
-        {
-            radius = scanner.nextDouble();
-            area = Math.PI * Math.pow(radius, 2);
-        
-            System.out.printf("%f", area);
-        }
+        radius = this.scanner.nextDouble();
+        area = Math.PI * Math.pow(radius, 2);
+
+        System.out.printf("%f\n", area);
     }
 
-
-    public static void ex_r_15()
-    {
+    public void ex_r_15() {
         double price0, perc_prof, perc_tax, price, prof, tax;
 
-        try(Scanner scanner = new Scanner(System.in))
-        {
-            price0 = scanner.nextDouble();
-            perc_prof = scanner.nextDouble();
-            perc_tax = scanner.nextDouble();
+        price0 = this.scanner.nextDouble();
+        perc_prof = this.scanner.nextDouble();
+        perc_tax = this.scanner.nextDouble();
 
-            prof = price0 * (perc_prof / 100);
-            tax = price0 * (perc_tax / 100);
-            price = price0 + prof + tax;
+        prof = price0 * (perc_prof / 100);
+        tax = price0 * (perc_tax / 100);
+        price = price0 + prof + tax;
 
-            System.out.printf("%f", price);
-        }
+        System.out.printf("%f\n", price);
     }
 
-
-    public static void ex_r_20()
-    {
+    public void ex_r_20() {
         double deg, h, rad, l;
 
-        try(Scanner scanner = new Scanner(System.in))
-        {
-            deg = scanner.nextDouble();
-            h = scanner.nextDouble();
-            rad = Math.toRadians(deg);
-            l = h * Math.sin(rad);
+        deg = this.scanner.nextDouble();
+        h = this.scanner.nextDouble();
+        rad = Math.toRadians(deg);
+        l = h * Math.sin(rad);
 
-            System.out.printf("%f", l);
-        }
+        System.out.printf("%f\n", l);
     }
 
-
-    public static void ex_r_25()
-    {
+    public void ex_r_25() {
         double price_total, price_ticket;
         int n;
 
-        try(Scanner scanner = new Scanner(System.in))
-        {
-            price_total = scanner.nextDouble();
-            price_ticket = scanner.nextDouble();
-            n = (int)Math.ceil(price_total / price_ticket);
+        price_total = this.scanner.nextDouble();
+        price_ticket = this.scanner.nextDouble();
+        n = (int) Math.ceil(price_total / price_ticket);
 
-            System.out.printf("%d", n);
-        }
+        System.out.printf("%d\n", n);
     }
 
+    public void ex_p_05() {
+        double price, nprice;
 
-    public static void main(String[] args)
-    {
-        ex_r_1();
-        ex_r_5();
-        ex_r_10();
-        ex_r_15();
-        ex_r_20();
-        ex_r_25();
+        price = this.scanner.nextDouble();
+        nprice = price * 0.9;
+
+        System.out.printf("%f\n", nprice);
+    }
+
+    public void ex_p_10() {
+        double l, area;
+
+        l = this.scanner.nextDouble();
+        area = Math.pow(l, 2);
+
+        System.out.printf("%f\n", area);
+    }
+
+    public void ex_p_15() {
+        double b1, b2, sal;
+
+        b1 = this.scanner.nextDouble();
+        b2 = this.scanner.nextDouble();
+        sal = this.scanner.nextDouble();
+
+        sal -= 1.02 * (b1 + b2);
+
+        System.out.printf("%f\n", sal);
+    }
+
+    public void ex_p_20() {
+        double deg, dis, rad, l;
+
+        deg = this.scanner.nextDouble();
+        dis = this.scanner.nextDouble();
+
+        rad = Math.toRadians(deg);
+        l = dis / Math.cos(rad);
+
+        System.out.printf("%f\n", l);
+    }
+
+    public void ex_p_25() {
+        double hour, min, conv, tmin, sec;
+
+        hour = this.scanner.nextDouble();
+        min = this.scanner.nextDouble();
+        conv = hour * 60;
+        tmin = conv + min;
+        sec = tmin * 60;
+
+        System.out.printf("%f\n", conv);
+        System.out.printf("%f\n", tmin);
+        System.out.printf("%f\n", sec);
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        main.ex_r_05();
+        main.ex_r_10();
+        main.ex_r_15();
+        main.ex_r_20();
+        main.ex_r_25();
+        main.ex_p_05();
+        main.ex_p_10();
+        main.ex_p_15();
+        main.ex_p_20();
+        main.ex_p_25();
+
+        main.close();
     }
 };
