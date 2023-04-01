7d28da04845df9d45064d025cba01411cceb55da
João Vaz <119084806+jg2alv@users.noreply.github.com>
2023-04-01 12:08:21 -0300

    Add proposed exercises

--- aeds1/livros/Fundamentos_da_Programacao_de_Computadores/Cap_3/C.c
+++ aeds1/livros/Fundamentos_da_Programacao_de_Computadores/Cap_3/C.c
@@ -2,25 +2,10 @@
 #include <math.h>
 
 #ifndef M_PI
-    #define M_PI 3.1415
+#define M_PI 3.1415
 #endif
 
-void ex_r_1()
-{
-    double n1, n2, n3, n4, sum;
-
-    scanf("%lf", &n1);
-    scanf("%lf", &n2);
-    scanf("%lf", &n3);
-    scanf("%lf", &n4);
-
-    sum = n1 + n2 + n3 + n4;
-
-    printf("%lf\n", sum);
-}
-
-
-void ex_r_5()
+void ex_r_05()
 {
     double sal, perc, nsal;
 
@@ -29,9 +14,8 @@ void ex_r_5()
 
     nsal = sal * (perc / 100);
 
-    printf("%lf", nsal);
+    printf("%lf\n", nsal);
 }
-
 
 void ex_r_10()
 {
@@ -41,9 +25,8 @@ void ex_r_10()
 
     area = M_PI * pow(radius, 2);
 
-    printf("%lf", area);
+    printf("%lf\n", area);
 }
-
 
 void ex_r_15()
 {
@@ -57,9 +40,8 @@ void ex_r_15()
     tax = price0 * (perc_tax / 100);
     price = price0 + prof + tax;
 
-    printf("%lf %lf %lf", prof, tax, price);
+    printf("%lf %lf %lf\n", prof, tax, price);
 }
-
 
 void ex_r_20()
 {
@@ -71,9 +53,8 @@ void ex_r_20()
     rad = deg * M_PI / 180;
     l = h * sin(rad);
 
-    printf("%lf", l);
+    printf("%lf\n", l);
 }
-
 
 void ex_r_25()
 {
@@ -85,18 +66,85 @@ void ex_r_25()
 
     n = ceil(price_total / price_ticket);
 
-    printf("%d", n);
+    printf("%d\n", n);
 }
 
+void ex_p_05()
+{
+    double price, nprice;
+
+    scanf("%lf", &price);
+
+    nprice = price * 0.9;
+
+    printf("%lf\n", nprice);
+}
+
+void ex_p_10()
+{
+    double l, area;
+
+    scanf("%lf", &l);
+
+    area = pow(l, 2);
+
+    printf("%lf\n", area);
+}
+
+void ex_p_15()
+{
+    double b1, b2, sal;
+
+    scanf("%lf", &b1);
+    scanf("%lf", &b2);
+    scanf("%lf", &sal);
+
+    sal -= 1.02 * (b1 + b2);
+
+    printf("%lf\n", sal);
+}
+
+void ex_p_20()
+{
+    double deg, dis, rad, l;
+
+    scanf("%lf", &deg);
+    scanf("%lf", &dis);
+
+    rad = deg * M_PI / 180;
+    l = dis / cos(rad);
+
+    printf("%lf\n", l);
+}
+
+void ex_p_25()
+{
+    double hour, min, conv, tmin, sec;
+
+    scanf("%lf", &hour);
+    scanf("%lf", &min);
+
+    conv = hour * 60;
+    tmin = conv + min;
+    sec = tmin * 60;
+
+    printf("%lf\n", conv);
+    printf("%lf\n", tmin);
+    printf("%lf\n", sec);
+}
 
 int main()
 {
-    ex_r_1();
-    ex_r_5();
+    ex_r_05();
     ex_r_10();
     ex_r_15();
     ex_r_20();
     ex_r_25();
+    ex_p_05();
+    ex_p_10();
+    ex_p_15();
+    ex_p_20();
+    ex_p_25();
 
     return 0;
 }
