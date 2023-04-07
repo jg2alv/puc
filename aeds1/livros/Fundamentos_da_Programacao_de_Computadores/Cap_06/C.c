988853456eeee8a277f57ba0b08d7bee68891c46
João Vaz <119084806+jg2alv@users.noreply.github.com>
2023-04-07 10:42:55 -0300

    Add solutions for chapt. 6 in java and C

--- aeds1/livros/Fundamentos_da_Programacao_de_Computadores/Cap_06/C.c
+++ aeds1/livros/Fundamentos_da_Programacao_de_Computadores/Cap_06/C.c
@@ -5,68 +5,137 @@
 #define M_PI 3.1415
 #endif
 
-void ex_r_05()
-{
-
-}
-
 void ex_r_10()
 {
+    char gab[8];
+    int pts = 0;
+    int ap = 0;
 
+    for (int i = 0; i < 8; i++)
+        scanf(" %c", &gab[i]);
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            char ans;
+            scanf(" %c", &ans);
+            if (ans == gab[i])
+                pts++;
+        }
+
+        if (pts >= 6)
+            ap++;
+
+        pts = 0;
+    }
+
+    printf("percentual de aprovados: %d\n", ap * 10);
 }
 
 void ex_r_15()
 {
+    int nums[10];
+    int divs[5];
 
+    for (int i = 0; i < 10; i++)
+        scanf("%d", &nums[i]);
+
+    for (int i = 0; i < 5; i++)
+        scanf("%d", &divs[i]);
+
+    for (int i = 0; i < 10; i++)
+    {
+        int n = nums[i];
+        int div = 0;
+        printf("Numero %d\n", n);
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (n % divs[i] == 0)
+            {
+                div = 1;
+                printf("Divisivel por %d na posicao %d", divs[i], i);
+            }
+        }
+
+        if (!div)
+            printf("Nao possui divisores no segundo vetor\n");
+    }
 }
 
 void ex_r_20()
 {
-    
+    double nums[5];
+
+    for (int i = 0; i < 5; i++)
+        scanf("%d", &nums[i]);
+
+    int code;
+    scanf("%d", &code);
+
+    switch (code)
+    {
+    case 0:
+        break;
+
+    case 1:
+        for (int i = 4; i >= 0; i--)
+            printf("%f ", nums[i]);
+
+    case 2:
+        for (int i = 0; i < 5; i++)
+            printf("%f ", nums[i]);
+
+    default:
+        printf("Opcao invalida!");
+        break;
+    }
+
+    printf("\n");
 }
 
 void ex_r_25()
 {
+    int primes[10];
+    int prime = 1;
+    int count = 101;
+    int i = 0;
+    int len = sizeof(primes) / sizeof(int);
 
-}
+    while (i < len)
+    {
+        for (int j = 2; j < ceil(sqrt(count)); j++)
+        {
+            if (count % j == 0)
+            {
+                prime = 0;
+                break;
+            }
+        }
 
-void ex_p_05()
-{
-    
-}
+        if (prime)
+        {
+            primes[i] = count;
+            i++;
+        }
 
-void ex_p_10()
-{
-    
-}
+        count++;
+        prime = 1;
+    }
 
-void ex_p_15()
-{
-    
-}
+    for (i = 0; i < len; i++)
+        printf("%d ", primes[i]);
 
-void ex_p_20()
-{
-    
-}
-
-void ex_p_25()
-{
-    
+    printf("\n");
 }
 
 int main()
 {
-    ex_r_05();
     ex_r_10();
     ex_r_15();
     ex_r_20();
     ex_r_25();
-    ex_p_05();
-    ex_p_10();
-    ex_p_15();
-    ex_p_20();
-    ex_p_25();
 
     return 0;
 }
--- aeds1/livros/Fundamentos_da_Programacao_de_Computadores/Cap_06/Java.java
+++ aeds1/livros/Fundamentos_da_Programacao_de_Computadores/Cap_06/Java.java
@@ -1,4 +1,5 @@
 package aeds1.livros.Fundamentos_da_Programacao_de_Computadores.Cap_06;
+
 import java.util.*;
 
 class Main {
@@ -12,60 +13,122 @@ class Main {
         this.scanner.close();
     }
 
-    public void ex_r_05() {
-
-    }
-
     public void ex_r_10() {
-        
+        String[] gab = new String[8];
+        int pts = 0;
+        int ap = 0;
+
+        for (int i = 0; i < 8; i++)
+            gab[i] = this.scanner.nextLine();
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 8; j++) {
+                String ans = this.scanner.nextLine();
+                if (ans == gab[i])
+                    pts++;
+            }
+
+            if (pts >= 6)
+                ap++;
+
+            pts = 0;
+        }
+
+        System.out.printf("Percentual de alunos aprovados: %d%", ap * 10);
     }
 
     public void ex_r_15() {
-        
+        int[] nums = new int[10];
+        int[] divs = new int[5];
+
+        for (int i = 0; i < 10; i++)
+            nums[i] = this.scanner.nextInt();
+
+        for (int i = 0; i < 10; i++)
+            divs[i] = this.scanner.nextInt();
+
+        for (int n : nums) {
+            boolean div = false;
+            System.out.printf("Numero %d\n", n);
+
+            for (int i = 0; i < 5; i++) {
+                if (n % divs[i] == 0) {
+                    div = true;
+                    System.out.printf("Divisivel por %d na posicao %d", divs[i], i);
+                }
+            }
+
+            if (!div)
+                System.out.printf("Nao possui divisores no segundo vetor\n");
+        }
     }
 
     public void ex_r_20() {
-        
+        double[] nums = new double[5];
+
+        for (int i = 0; i < 5; i++)
+            nums[i] = this.scanner.nextDouble();
+
+        int code = this.scanner.nextInt();
+        switch (code) {
+            case 0:
+                break;
+
+            case 1:
+                for (int i = 4; i >= 0; i--)
+                    System.out.printf("%f ", nums[i]);
+                break;
+
+            case 2:
+                for (int i = 0; i < 5; i++)
+                    System.out.printf("%f ", nums[i]);
+                break;
+
+            default:
+                System.out.println("Opcao invalida!");
+                break;
+        }
+
+        System.out.println("");
     }
 
     public void ex_r_25() {
-        
-    }
+        int[] primes = new int[10];
+        boolean prime = true;
+        int count = 101;
+        int i = 0;
 
-    public void ex_p_05() {
-        
-    }
+        while (i < primes.length) {
+            for (int j = 2; j < Math.ceil(Math.sqrt(count)); j++) {
+                if (count % j == 0) {
+                    prime = false;
+                    break;
+                }
+            }
 
-    public void ex_p_10() {
-        
-    }
+            if (prime) {
+                primes[i] = count;
+                i++;
+            }
 
-    public void ex_p_15() {
-        
-    }
+            count++;
+            prime = true;
+        }
 
-    public void ex_p_20() {
-        
-    }
+        for (i = 0; i < primes.length; i++)
+            System.out.printf("%d ", primes[i]);
 
-    public void ex_p_25() {
-        
+        System.out.println("");
     }
 
     public static void main(String[] args) {
         Main main = new Main();
 
-        main.ex_r_05();
         main.ex_r_10();
         main.ex_r_15();
         main.ex_r_20();
         main.ex_r_25();
-        main.ex_p_05();
-        main.ex_p_10();
-        main.ex_p_15();
-        main.ex_p_20();
-        main.ex_p_25();
-        
+
         main.close();
     }
 }
