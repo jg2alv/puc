e28d66c92a217e3628a597431f9974fa2169282e
João Vaz <119084806+jg2alv@users.noreply.github.com>
2023-04-08 16:11:08 -0300

    Add solutions for chapt. 7

--- aeds1/livros/Fundamentos_da_Programacao_de_Computadores/Cap_07/C.c
+++ aeds1/livros/Fundamentos_da_Programacao_de_Computadores/Cap_07/C.c
@@ -7,66 +7,150 @@
 
 void ex_r_05()
 {
+    const int mat[4][7];
+    int sml = 999999;
+    int grt = -99999;
+    int row = 0;
+    int col = 0;
 
+    const int ROWS = sizeof(mat) / sizeof(int);
+    const int COLS = sizeof(mat[0]) / sizeof(int);
+
+    for (int r = 0; r < ROWS; r++)
+    {
+        for (int c = 0; c < COLS; c++)
+        {
+            scanf("%d", &mat[r][c]);
+        }
+    }
+
+    for (int r = 0; r < ROWS; r++)
+    {
+        for (int c = 0; c < COLS; c++)
+        {
+            int curr = mat[r][c];
+
+            if (curr < sml)
+            {
+                row = r;
+                sml = curr;
+            }
+        }
+    }
+
+    for (int c = 0; c < COLS; c++)
+    {
+        int curr = mat[row][c];
+        if (curr > grt)
+        {
+            grt = curr;
+            col = c;
+        }
+    }
+
+    printf("\nLinha: %d\nColuna: %d\n", row + 1, col + 1);
 }
 
 void ex_r_10()
 {
+    int mat[10][10];
+    int temp;
 
+    const int ROWS = sizeof(mat) / sizeof(int);
+    const int COLS = sizeof(mat[0]) / sizeof(int);
+
+    for (int r = 0; r < ROWS; r++)
+    {
+        for (int c = 0; c < COLS; c++)
+        {
+            scanf("%d", &mat[r][c]);
+        }
+    }
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        temp = mat[1][i];
+        mat[1][i] = mat[7][i];
+        mat[7][i] = temp;
+    }
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        temp = mat[i][3];
+        mat[i][3] = mat[i][9];
+        mat[i][9] = temp;
+    }
+
+    for (int i = 0, j = ROWS - 1; i < ROWS; i++, j--)
+    {
+        temp = mat[i][i];
+        mat[i][i] = mat[i][j];
+        mat[i][j] = temp;
+    }
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        temp = mat[4][i];
+        mat[4][i] = mat[i][9];
+        mat[i][9] = temp;
+    }
 }
 
-void ex_r_15()
+void ex_p_17()
 {
+    int mat[10][10];
+    double avg = 0;
 
+    const int ROWS = sizeof(mat) / sizeof(int);
+    const int COLS = sizeof(mat[0]) / sizeof(int);
+
+    for (int r = 0; r < ROWS; r++)
+    {
+        for (int c = 0; c < COLS; c++)
+        {
+            scanf("%d", &mat[r][c]);
+        }
+    }
+
+    for (int i = 0; i < ROWS; i++)
+        avg += mat[i][i];
+
+    printf("Average: %f\n", avg / ROWS);
 }
 
-void ex_r_20()
+void ex_p_21()
 {
-    
-}
+    int mat[5][5];
 
-void ex_r_25()
-{
+    const int ROWS = sizeof(mat) / sizeof(int);
+    const int COLS = sizeof(mat[0]) / sizeof(int);
 
-}
+    for (int r = 0; r < ROWS; r++)
+    {
+        for (int c = 0; c < COLS; c++)
+        {
+            scanf("%d", &mat[r][c]);
+        }
+    }
 
-void ex_p_05()
-{
-    
-}
+    for (int r = 0; r < ROWS; r++)
+    {
+        for (int c = 0; c < COLS; c++)
+        {
+            mat[r][c] *= mat[r][r];
+            printf("%d ", mat[r][c]);
+        }
 
-void ex_p_10()
-{
-    
-}
-
-void ex_p_15()
-{
-    
-}
-
-void ex_p_20()
-{
-    
-}
-
-void ex_p_25()
-{
-    
+        printf("\n");
+    }
 }
 
 int main()
 {
     ex_r_05();
     ex_r_10();
-    ex_r_15();
-    ex_r_20();
-    ex_r_25();
-    ex_p_05();
-    ex_p_10();
-    ex_p_15();
-    ex_p_20();
-    ex_p_25();
+    ex_p_17();
+    ex_p_21();
 
     return 0;
 }
--- aeds1/livros/Fundamentos_da_Programacao_de_Computadores/Cap_07/Java.java
+++ aeds1/livros/Fundamentos_da_Programacao_de_Computadores/Cap_07/Java.java
@@ -1,4 +1,5 @@
 package aeds1.livros.Fundamentos_da_Programacao_de_Computadores.Cap_07;
+
 import java.util.*;
 
 class Main {
@@ -13,43 +14,108 @@ class Main {
     }
 
     public void ex_r_05() {
+        final int[][] mat = new int[4][7];
+        int sml = Integer.MAX_VALUE;
+        int grt = Integer.MIN_VALUE;
+        int row = 0;
+        int col = 0;
 
+        for (int r = 0; r < mat.length; r++) {
+            for (int c = 0; c < mat[0].length; c++) {
+                mat[r][c] = this.scanner.nextInt();
+            }
+        }
+
+        for (int r = 0; r < mat.length; r++) {
+            for (int c = 0; c < mat[0].length; c++) {
+                int curr = mat[r][c];
+
+                if (curr < sml) {
+                    row = r;
+                    sml = curr;
+                }
+            }
+        }
+
+        for (int c = 0; c < mat[0].length; c++) {
+            int curr = mat[row][c];
+            if (curr > grt) {
+                grt = curr;
+                col = c;
+            }
+        }
+
+        System.out.printf("\nLinha: %d\nColuna: %d\n", row + 1, col + 1);
     }
 
     public void ex_r_10() {
-        
+        int[][] mat = new int[10][10];
+        int temp;
+
+        for (int r = 0; r < mat.length; r++) {
+            for (int c = 0; c < mat[0].length; c++) {
+                mat[r][c] = this.scanner.nextInt();
+            }
+        }
+
+        for (int i = 0; i < mat.length; i++) {
+            temp = mat[1][i];
+            mat[1][i] = mat[7][i];
+            mat[7][i] = temp;
+        }
+
+        for (int i = 0; i < mat.length; i++) {
+            temp = mat[i][3];
+            mat[i][3] = mat[i][9];
+            mat[i][9] = temp;
+        }
+
+        for (int i = 0, j = mat.length - 1; i < mat.length; i++, j--) {
+            temp = mat[i][i];
+            mat[i][i] = mat[i][j];
+            mat[i][j] = temp;
+        }
+
+        for (int i = 0; i < mat.length; i++) {
+            temp = mat[4][i];
+            mat[4][i] = mat[i][9];
+            mat[i][9] = temp;
+        }
     }
 
-    public void ex_r_15() {
-        
+    public void ex_p_17() {
+        int[][] mat = new int[10][10];
+        double avg = 0;
+
+        for (int r = 0; r < mat.length; r++) {
+            for (int c = 0; c < mat[0].length; c++) {
+                mat[r][c] = this.scanner.nextInt();
+            }
+        }
+
+        for(int i = 0; i < mat.length; i++)
+            avg += mat[i][i];
+
+        System.out.printf("Average: %f\n", avg / mat.length);
     }
 
-    public void ex_r_20() {
-        
-    }
+    public void ex_p_21() {
+        int[][] mat = new int[5][5];
 
-    public void ex_r_25() {
-        
-    }
+        for (int r = 0; r < mat.length; r++) {
+            for (int c = 0; c < mat[0].length; c++) {
+                mat[r][c] = this.scanner.nextInt();
+            }
+        }
 
-    public void ex_p_05() {
-        
-    }
+        for (int r = 0; r < mat.length; r++) {
+            for (int c = 0; c < mat[0].length; c++) {
+                mat[r][c] *= mat[r][r];
+                System.out.printf("%d ", mat[r][c]);
+            }
 
-    public void ex_p_10() {
-        
-    }
-
-    public void ex_p_15() {
-        
-    }
-
-    public void ex_p_20() {
-        
-    }
-
-    public void ex_p_25() {
-        
+            System.out.println();
+        } 
     }
 
     public static void main(String[] args) {
@@ -57,15 +123,9 @@ class Main {
 
         main.ex_r_05();
         main.ex_r_10();
-        main.ex_r_15();
-        main.ex_r_20();
-        main.ex_r_25();
-        main.ex_p_05();
-        main.ex_p_10();
-        main.ex_p_15();
-        main.ex_p_20();
-        main.ex_p_25();
-        
+        main.ex_p_17();
+        main.ex_p_21();
+
         main.close();
     }
 }
