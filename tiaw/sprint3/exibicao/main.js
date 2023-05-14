e1d34e28cec6058d428e3339078eb87ed029c2f4
João Vaz <119084806+jg2alv@users.noreply.github.com>
2023-05-14 09:16:47 -0300

    Move sprint3 to separate folder

---
+++ tiaw/sprint3/exibicao/main.js
@@ -0,0 +1,40 @@
+(function () {
+    if (localStorage.getItem('PETS_ADOCAO') === null) return;
+    document.querySelector('#nothing').remove();
+
+    const ROWS = 3;
+
+    const cnt = document.querySelector('#container');
+    const pets = JSON.parse(localStorage.getItem('PETS_ADOCAO'));
+    const numrows = Math.ceil(pets.length / ROWS);
+    const rows = [];
+
+
+    for (let i = 0; i < numrows; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row', 'row-cols-3');
+        cnt.appendChild(row);
+        rows.push(row);
+    }
+
+    pets.forEach((pet, i) => {
+        const col = document.createElement('div');
+        const idx = Math.floor(i / ROWS);
+
+        col.classList.add('col', 'btn');
+        col.style.backgroundImage = `url(${pet['photo']})`;
+        col.innerHTML = `<div class='list-group'><div class='list-group-item list-group-item-action flex-column align-items-start active'><div class='d-flex w-100 justify-content-between'><h5 class='mb-1'>${pet['name']}</h5><small>${pet['age']}</small></div><p class='mb-1'>${pet['desc']}</p><small>${pet['species']}</small></div></div>`
+
+        col.addEventListener('mouseover', () => {
+            col.firstChild.style.display = 'block';
+            col.style.backgroundImage = 'none';
+        });
+
+        col.addEventListener('mouseout', () => {
+            col.firstChild.style.display = 'none';
+            col.style.backgroundImage = `url(${pet['photo']})`;
+        });
+
+        rows[idx].appendChild(col);
+    });
+})();
