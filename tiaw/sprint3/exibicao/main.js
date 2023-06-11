ad17450aeed0e019be55149b43bbd2bf10ea0ab7
João Vaz <119084806+jg2alv@users.noreply.github.com>
2023-06-11 17:38:26 -0300

    Minor modif

--- tiaw/sprint3/exibicao/main.js
+++ tiaw/sprint3/exibicao/main.js
@@ -1,6 +1,8 @@
 (function () {
-    if (localStorage.getItem('PETS_ADOCAO') === null) return;
+    // if (localStorage.getItem('PETS_ADOCAO') === null) return;
     document.querySelector('#nothing').remove();
+
+    localStorage.setItem('PETS_ADOCAO', '[{"name":"Black","age":6,"spec":"shih-tzu","desc":"Black, o cachorro branco","photo":"https://diariodonordeste.verdesmares.com.br/image/contentid/policy:7.4516804:1621518947/Shih-Tzu.jpg?f=default&$p$f=88cd607"},{"name":"Black","age":6,"spec":"shih-tzu","desc":"Black, o cachorro branco","photo":"https://diariodonordeste.verdesmares.com.br/image/contentid/policy:7.4516804:1621518947/Shih-Tzu.jpg?f=default&$p$f=88cd607"},{"name":"Black","age":6,"spec":"shih-tzu","desc":"Black, o cachorro branco","photo":"https://diariodonordeste.verdesmares.com.br/image/contentid/policy:7.4516804:1621518947/Shih-Tzu.jpg?f=default&$p$f=88cd607"},{"name":"Black","age":6,"spec":"shih-tzu","desc":"Black, o cachorro branco","photo":"https://diariodonordeste.verdesmares.com.br/image/contentid/policy:7.4516804:1621518947/Shih-Tzu.jpg?f=default&$p$f=88cd607"},{"name":"Black","age":6,"spec":"shih-tzu","desc":"Black, o cachorro branco","photo":"https://diariodonordeste.verdesmares.com.br/image/contentid/policy:7.4516804:1621518947/Shih-Tzu.jpg?f=default&$p$f=88cd607"},{"name":"Black","age":6,"spec":"shih-tzu","desc":"Black, o cachorro branco","photo":"https://diariodonordeste.verdesmares.com.br/image/contentid/policy:7.4516804:1621518947/Shih-Tzu.jpg?f=default&$p$f=88cd607"},{"name":"Black","age":6,"spec":"shih-tzu","desc":"Black, o cachorro branco","photo":"https://diariodonordeste.verdesmares.com.br/image/contentid/policy:7.4516804:1621518947/Shih-Tzu.jpg?f=default&$p$f=88cd607"},{"name":"Black","age":6,"spec":"shih-tzu","desc":"Black, o cachorro branco","photo":"https://diariodonordeste.verdesmares.com.br/image/contentid/policy:7.4516804:1621518947/Shih-Tzu.jpg?f=default&$p$f=88cd607"},{"name":"Black","age":6,"spec":"shih-tzu","desc":"Black, o cachorro branco","photo":"https://diariodonordeste.verdesmares.com.br/image/contentid/policy:7.4516804:1621518947/Shih-Tzu.jpg?f=default&$p$f=88cd607"}]')
 
     const ROWS = 3;
 
@@ -23,7 +25,7 @@
 
         col.classList.add('col', 'btn');
         col.style.backgroundImage = `url(${pet['photo']})`;
-        col.innerHTML = `<div class='list-group'><div class='list-group-item list-group-item-action flex-column align-items-start active'><div class='d-flex w-100 justify-content-between'><h5 class='mb-1'>${pet['name']}</h5><small>${pet['age']}</small></div><p class='mb-1'>${pet['desc']}</p><small>${pet['species']}</small></div></div>`
+        col.innerHTML = `<div class='list-group'><div class='list-group-item list-group-item-action flex-column align-items-start active'><div class='d-flex w-100 justify-content-between'><h5 class='mb-1'>${pet['name']}</h5><small>${pet['age']}</small></div><p class='mb-1'>${pet['desc']}</p><small>${pet['spec']}</small></div></div>`
 
         col.addEventListener('mouseover', () => {
             col.firstChild.style.display = 'block';
